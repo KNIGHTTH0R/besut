767bc92f162c4074f7599efc3a1d115df0e98909
Muhammad Shodiq <iDevoid@users.noreply.github.com>
2017-10-01 05:57:51 +0700

    Facebook and Google Login

--- besut-website/application/views/login.php
+++ besut-website/application/views/login.php
@@ -1,19 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Facebook Login JavaScript Example</title>
+<meta charset="UTF-8">
+<meta name="google-signin-client_id" content="540463499518-ns8njfhs138fvrg1blfagmgq6vpl2d7m.apps.googleusercontent.com">
+</head>
 <body>
-  <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="true" data-auto-logout-link="true" data-use-continue-as="true" onlogin="checkLoginState();"></div>
+
+<div id="status"></div>
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
+<a href="https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost/benangkusut" onclick="signOut();">Sign out</a>
 </body>
-<script src="../scripts/fbSDK.js"></script>
-<div id="fb-root"></div>
-<script>
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.10&appId=517776231890588";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-      function checkLoginState() {
-        FB.getLoginStatus(function(response) {
-          statusChangeCallback(response);
-        });
-      }
-</script>
+<script src="scripts/APIs/facebookAPI.js"></script>
+<script src="scripts/APIs/googleAPI.js"></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+</html>
